3eb1072991ef56f06b64c0361840e60a122b7777
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2024-02-07 16:11:11 +0000

    home default

--- frontend/src/utils/Router.js
+++ frontend/src/utils/Router.js
@@ -6,8 +6,7 @@ import SSIProfilePagePage from '../pages/ssi-profile'
 import NewHomePage from '../pages/home'
 
 export const router = createBrowserRouter([
-  { path: '*', element: <>hello</> },
-  { path: '/home', element: <NewHomePage /> },
+  { path: '*', element: <NewHomePage /> },
   { path: '/sign-up', element: <SignUpPage /> },
   { path: '/complete-onboarding', element: <CompleteOnboardingPage /> },
   { path: '/ssi-profile', element: <SSIProfilePagePage /> },

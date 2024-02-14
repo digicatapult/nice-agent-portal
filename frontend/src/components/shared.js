e0be3c19aac29ec65e44e6621ed3a1071fb1431d
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2024-02-14 10:51:26 +0000

    initial commit

---
+++ frontend/src/components/shared.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+
+export const Button = styled('button')`
+  height: 30px;
+  border-radius: 8px;
+  border: 1px solid #000;
+  padding: 0px 15px;
+  background: #fff;
+  margin-right: 3px;
+  box-shadow: 0px 2px 0px 0px #000;
+`
+
+export const Input = styled('input')`
+  border-radius: 3px;
+  width: 100%;
+  height: 3em;
+  font-size: 1em;
+  margin: 5px 0px 10px 0px;
+  text-align: center;
+  border: 2px solid var(--black, #000);
+  background: var(--white, #fff);
+`
+
+export const SmallText = styled.p`
+  margin: 0px;
+  font-weight: bold;
+`
+export const HeadingText = styled.h1`
+  margin: 10px 0px 50px 0px;
+  text-align: center;
+`
+
+export const ContainerStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: Roboto, sans-serif;
+`
+
+export const SmallItallicisedText = styled.p`
+  margin: 0px;
+  padding: 4px;
+  font-style: italic;
+  font-family: 'Roboto Thin', sans-serif;
+  color: black;
+  font-size: 14px;
+`
+
+export const SmallThinText = styled.p`
+  margin: 0px;
+  font-weight: regular;
+  font-size: 14px;
+  font-family: 'Roboto Thin', sans-serif;
+  font-weight: normal;
+`
+export const SmallCenteredThinText = styled.p`
+  margin: 0px;
+  font-weight: regular;
+  font-size: 14px;
+  font-family: 'Roboto Thin', sans-serif;
+  font-weight: normal;
+  text-align: center;
+`
--- frontend/src/pages/SSIProfile.js
+++ frontend/src/pages/SSIProfile.js
@@ -1,8 +1,62 @@
 import React from 'react'
+import { Section } from '@digicatapult/ui-component-library'
+import {
+  ContainerStyle,
+  HeadingText,
+  Button,
+  SmallThinText,
+} from '../components/shared'
 const SSIProfilePagePage = () => {
   return (
     <div>
-      <h1>SSI Profile</h1>
+      <ContainerStyle>
+        <Section
+          margin={'40px 0px 0px 0px'}
+          headingLevel={1}
+          background={'#FFF'}
+          title={'- NICE Network - '}
+        ></Section>
+        <HeadingText>SSI Profile Page</HeadingText>
+        <div>
+          <img src={'/images/ssi_profile.png'}></img>
+          <Button
+            type="submit"
+            style={{ marginTop: '25px', marginLeft: '2px' }}
+          >
+            Share my NICE id
+          </Button>
+        </div>
+        <Section headingLevel={1} title="Your Profile" background={'#FFF'}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{ display: 'flex' }}>
+              <b>Your Company Name: </b>
+              <SmallThinText>Sam's Batteries</SmallThinText>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <b>Companies House Number: </b>
+              <SmallThinText>12345678</SmallThinText>
+            </div>
+            <div style={{ display: 'flex' }}>
+              <b>Contact Email: </b>
+              <SmallThinText>sam@batterie.xyz</SmallThinText>
+            </div>
+          </div>
+          <div style={{ border: '2px solid black ' }}>
+            <Section
+              headingLevel={1}
+              background={'#FFF'}
+              border={'2px solid black'}
+              title={'Your Profile is verified by the nice network '}
+            ></Section>
+          </div>
+        </Section>
+        <Button type="submit" style={{ marginTop: '25px', marginLeft: '2px' }}>
+          View your NICE connections
+        </Button>
+        <Button type="submit" style={{ marginTop: '25px', marginLeft: '2px' }}>
+          Add connection
+        </Button>
+      </ContainerStyle>
     </div>
   )
 }

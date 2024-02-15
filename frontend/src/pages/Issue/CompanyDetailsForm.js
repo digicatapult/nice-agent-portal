e39fdf6b648086c607d4bd1298d1a3d433e3f952
Jonathan Gray <jonmattgray@gmail.com>
2024-02-15 13:37:26 +0000

    QR code scan (#13)
    * wip
    
    * success page
    
    * shared header
    
    * shared header
    
    * revert shared comp changes

--- frontend/src/pages/Issue/CompanyDetailsForm.js
+++ frontend/src/pages/Issue/CompanyDetailsForm.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Section } from '@digicatapult/ui-component-library'
 import {
   Input,
   Button,
   HeadingText,
   SmallItallicisedText,
-  ContainerStyle,
 } from '../../components/shared'
+import WrapperWithHeader from '../../components/Header'
 
 export default function CompanyDetailsForm({
   handleSubmit,
@@ -22,75 +21,67 @@ export default function CompanyDetailsForm({
     }))
   }
   return (
-    <div>
-      <ContainerStyle>
-        <Section
-          margin={'40px 0px 0px 0px'}
-          headingLevel={1}
-          background={'#FFF'}
-          title={'- NICE Network - '}
-        ></Section>
-        <HeadingText>Welcome to the NICE Network</HeadingText>
-        <div
-          style={{
-            flex: '1',
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <form style={{ textAlign: 'center' }} onSubmit={handleSubmit}>
-            <SmallItallicisedText>Your Company Name</SmallItallicisedText>
-            <Input
-              type="text"
-              required
-              name="name"
-              value={inputs.name}
-              placeholder="Company name"
-              onChange={handleChange}
-            />
-            <SmallItallicisedText>
-              Enter your Companies House Number
-            </SmallItallicisedText>
-            <Input
-              required
-              type="text"
-              pattern="^[a-zA-Z0-9]{8}$"
-              name={'houseNo'}
-              value={inputs.houseNo}
-              placeholder="Company house no."
-              onChange={handleChange}
-              title="Eight alphanumeric characters."
-            />
-            <SmallItallicisedText>
-              We will send your onboarding QR code here
-            </SmallItallicisedText>
-            <Input
-              type="email"
-              required
-              value={inputs.email}
-              name="email"
-              placeholder="Contact email"
-              onChange={handleChange}
-            />
-            <div>
-              <Button
-                onClick={(e) => {
-                  handleBackToHomepage(e)
-                }}
-              >
-                {'<'}
-              </Button>
-              <Button
-                type="submit"
-                style={{ marginTop: '25px', marginLeft: '2px' }}
-              >
-                Next
-              </Button>
-            </div>
-          </form>
-        </div>
-      </ContainerStyle>
-    </div>
+    <WrapperWithHeader>
+      <HeadingText>Welcome to the NICE Network</HeadingText>
+      <div
+        style={{
+          flex: '1',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <form style={{ textAlign: 'center' }} onSubmit={handleSubmit}>
+          <SmallItallicisedText>Your Company Name</SmallItallicisedText>
+          <Input
+            type="text"
+            required
+            name="name"
+            value={inputs.name}
+            placeholder="Company name"
+            onChange={handleChange}
+          />
+          <SmallItallicisedText>
+            Enter your Companies House Number
+          </SmallItallicisedText>
+          <Input
+            required
+            type="text"
+            pattern="^[a-zA-Z0-9]{8}$"
+            name={'houseNo'}
+            value={inputs.houseNo}
+            placeholder="Company house no."
+            onChange={handleChange}
+            title="Eight alphanumeric characters."
+          />
+          <SmallItallicisedText>
+            We will send your onboarding QR code here
+          </SmallItallicisedText>
+          <Input
+            type="email"
+            required
+            value={inputs.email}
+            name="email"
+            placeholder="Contact email"
+            onChange={handleChange}
+          />
+          <div>
+            <Button
+              onClick={(e) => {
+                handleBackToHomepage(e)
+              }}
+            >
+              {'<'}
+            </Button>
+            <Button
+              type="submit"
+              style={{ marginTop: '25px', marginLeft: '2px' }}
+            >
+              Next
+            </Button>
+          </div>
+        </form>
+      </div>
+    </WrapperWithHeader>
   )
 }
--- frontend/src/pages/Issue/Summary.js
+++ frontend/src/pages/Issue/Summary.js
@@ -6,99 +6,84 @@ import {
   Button,
   SmallText,
   HeadingText,
-  ContainerStyle,
   SmallCenteredThinText,
   SmallThinText,
 } from '../../components/shared'
+import WrapperWithHeader from '../../components/Header'
 
 export default function Summary({ inputs, setStage }) {
   const InformationArea = styled.div`
     padding: 10px;
   `
   return (
-    <div>
-      <ContainerStyle>
+    <WrapperWithHeader>
+      <HeadingText>Welcome to the NICE Network</HeadingText>
+      <div
+        style={{
+          flex: '1',
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div style={{ textAlign: 'left', padding: '0px 20px' }}>
+          <SmallThinText style={{ paddingBottom: '20px' }}>
+            Review Your Details before submission:
+          </SmallThinText>
+          <InformationArea>
+            <SmallText>
+              <b>Company Name: </b>
+            </SmallText>
+            <SmallThinText>{inputs.name}</SmallThinText>
+          </InformationArea>
+          <InformationArea>
+            <SmallText>
+              <b>Company House No.: </b>
+            </SmallText>
+            <SmallThinText>{inputs.houseNo}</SmallThinText>
+          </InformationArea>
+          <InformationArea>
+            <SmallText>
+              <b>Company Email: </b>
+            </SmallText>
+            <SmallThinText>{inputs.email}</SmallThinText>
+          </InformationArea>
+        </div>
         <Section
-          margin={'40px 0px 0px 0px'}
+          margin={'2px 0px'}
           headingLevel={1}
           background={'#FFF'}
-          title={'- NICE Network - '}
-        ></Section>
-        <HeadingText>Welcome to the NICE Network</HeadingText>
-        <div
-          style={{
-            flex: '1',
-            alignItems: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
+          padding={'0px 40px 20px 40px'}
         >
-          <div style={{ textAlign: 'left', padding: '0px 20px' }}>
-            <SmallThinText style={{ paddingBottom: '20px' }}>
-              Review Your Details before submission:
-            </SmallThinText>
-            <InformationArea>
-              <SmallText>
-                <b>Company Name: </b>
-              </SmallText>
-              <SmallThinText>{inputs.name}</SmallThinText>
-            </InformationArea>
-            <InformationArea>
-              <SmallText>
-                <b>Company House No.: </b>
-              </SmallText>
-              <SmallThinText>{inputs.houseNo}</SmallThinText>
-            </InformationArea>
-            <InformationArea>
-              <SmallText>
-                <b>Company Email: </b>
-              </SmallText>
-              <SmallThinText>{inputs.email}</SmallThinText>
-            </InformationArea>
-          </div>
-          <Section
-            margin={'2px 0px'}
-            headingLevel={1}
-            background={'#FFF'}
-            padding={'0px 40px 20px 40px'}
-          >
-            <SmallCenteredThinText>
-              *Ensure your details are valid to receive the onboarding QR codes
-              from your mail (Companies House Number address) and contact email.
-            </SmallCenteredThinText>
-          </Section>
+          <SmallCenteredThinText>
+            *Ensure your details are valid to receive the onboarding QR codes
+            from your mail (Companies House Number address) and contact email.
+          </SmallCenteredThinText>
+        </Section>
 
-          <div>
-            {' '}
-            <Button
-              onClick={(e) => {
-                e.preventDefault()
-                setStage((prev) => prev - 1)
-              }}
-            >
-              {'<'}
-            </Button>
-            <Button
-              onClick={(e) => {
-                e.preventDefault()
-                setStage((prev) => prev + 1)
-                // eslint-disable-next-line
-                console.log(Math.floor(1000 + Math.random() * 9000).toString())
-                // should be doing sth to send the qr text
-              }}
-            >
-              {'Submit'}
-            </Button>
-            {/* <Button
-          onClick={(e) => {
-            handleSubmit(e, true)
-          }}
-        >
-          {'Submit'}
-        </Button> */}
-          </div>
+        <div>
+          {' '}
+          <Button
+            onClick={(e) => {
+              e.preventDefault()
+              setStage((prev) => prev - 1)
+            }}
+          >
+            {'<'}
+          </Button>
+          <Button
+            onClick={(e) => {
+              e.preventDefault()
+              setStage((prev) => prev + 1)
+              // eslint-disable-next-line
+              console.log(Math.floor(1000 + Math.random() * 9000).toString())
+              // should be doing sth to send the qr text
+            }}
+          >
+            {'Submit'}
+          </Button>
         </div>
-      </ContainerStyle>
-    </div>
+      </div>
+    </WrapperWithHeader>
   )
 }

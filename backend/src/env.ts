bc3fa906c224114dcd8f7c28fb8a951d1e3bb53a
Jonathan Gray <jonmattgray@gmail.com>
2024-02-22 09:51:13 +0000

    Add TSOA backend (#18)
    * add backend
    
    * serve both api and frontend
    
    * sort readmes
    
    * add tests to workflows
    
    * remove db
    
    * remove build step from test workflow

---
+++ backend/src/env.ts
@@ -0,0 +1,15 @@
+import * as envalid from 'envalid'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: 'test/test.env' })
+} else {
+  dotenv.config()
+}
+
+export default envalid.cleanEnv(process.env, {
+  PORT: envalid.port({ default: 3000 }),
+  LOG_LEVEL: envalid.str({ default: 'info', devDefault: 'debug' }),
+  CLOUDAGENT_HOST: envalid.host({ devDefault: 'localhost' }),
+  CLOUDAGENT_PORT: envalid.port({ default: 3001 }),
+})

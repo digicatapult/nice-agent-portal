769fbefa014af34e33ce33fafc0d7876f9691005
Jonathan Gray <jonmattgray@gmail.com>
2024-02-22 17:27:16 +0000

    use db envs

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,16 +1,19 @@
 import { singleton } from 'tsyringe'
 import { PrismaClient } from '@prisma/client'
 
-//import env from '../env.js'
+import env from '../env.js'
 
 @singleton()
 export default class Database {
   private db: PrismaClient
 
   constructor() {
-    this.db = new PrismaClient()
+    this.db = new PrismaClient({
+      datasourceUrl: `postgresql://${env.DB_USERNAME}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`,
+    })
   }
 
+  // example query
   getMembers = async () => {
     return this.db.member.findMany()
   }

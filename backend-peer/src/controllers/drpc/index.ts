c84b1e48e0a513a4db15fb582533b024e7b43fc4
Helena Adamkova <58051865+Ellenn-A@users.noreply.github.com>
2024-05-28 17:21:03 +0100

    added send-query endpoint

---
+++ backend-peer/src/controllers/drpc/index.ts
@@ -0,0 +1,56 @@
+import { Controller, Post, Route, SuccessResponse, Tags, Body } from 'tsoa'
+import { injectable } from 'tsyringe'
+
+import { logger } from '../../lib/logger.js'
+import { DrpcRequestObject, DrpcResponseObject } from './types.js'
+import { FetchAiAdapterManager } from '../../lib/services/adapter.js'
+
+const log = logger.child({ context: 'DrpcController' })
+
+@Route('api/drpc')
+@Tags('drpc')
+@injectable()
+export class DrpcController extends Controller {
+  constructor(private adapter: FetchAiAdapterManager) {
+    super()
+  }
+  /**
+   * @summary Receive query from external platform.
+   * @description Receive query from external platform (ex HyYield).
+   */
+  @SuccessResponse(204)
+  @Post('/receive-query')
+  public async receiveQuery(@Body() body: { drpcRequest: DrpcRequestObject }) {
+    log.debug({
+      msg: 'new request received',
+      controller: '/drpc',
+      payload: body,
+    })
+    const { drpcRequest } = body
+    await this.adapter.sendRequestToAdapter(drpcRequest)
+
+    //do sth here
+
+    return 'sth'
+  }
+  /**
+   * @summary Receive response from external platform.
+   * @description Receive response from external platform (ex HyYield).
+   */
+  @SuccessResponse(204)
+  @Post('/receive-response')
+  public async receiveResponse(
+    @Body() body: { did: string; drpcResponse: DrpcResponseObject }
+  ) {
+    log.debug({
+      msg: 'new request received',
+      controller: '/drpc',
+      payload: body,
+    })
+    const { drpcResponse } = body
+    console.log(drpcResponse)
+    //send response to postman
+
+    return 'sth'
+  }
+}
---
+++ backend-peer/src/controllers/drpc/types.ts
@@ -0,0 +1,51 @@
+export interface DrpcRequestObject {
+  jsonrpc: string
+  method: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  params?: any[] | object
+  id: string | number | null
+}
+
+export interface DrpcResponseObject {
+  jsonrpc: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  result?: any
+  error?: DrpcResponseError
+  id: string | number | null
+}
+export interface DrpcResponseError {
+  code: DrpcErrorCode
+  message: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data?: any
+}
+export enum DrpcErrorCode {
+  METHOD_NOT_FOUND = -32601,
+  PARSE_ERROR = -32700,
+  INVALID_REQUEST = -32600,
+  INVALID_PARAMS = -32602,
+  INTERNAL_ERROR = -32603,
+  SERVER_ERROR = -32000,
+}
+
+export interface DrpcEvent {
+  createdAt: Date
+  request?: DrpcRequestObject | DrpcRequestObject[]
+  response?: DrpcResponseObject[]
+  connectionId: string
+  role: DrpcRole
+  state: DrpcState
+  threadId: string
+  id: string
+  _tags: {}
+}
+export enum DrpcRole {
+  Client = 'client',
+  Server = 'server',
+}
+
+export enum DrpcState {
+  RequestSent = 'request-sent',
+  RequestReceived = 'request-received',
+  Completed = 'completed',
+}
